f29321c2c572fc1734f630ddda60d8581e9bc6e6
satinder <satindersingh472@gmail.com>
2022-07-30 11:10:14 -0600

    updated

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,12 @@
+function go_back(details){
+    location.href = `index.html`;
+}
 let button = document.getElementById(`logout`);
 let cookies_value = Cookies.get(`token`);
 if(cookies_value !== undefined){
 button[`style`][`display`] = `grid`;
 } else {
-    alert(`you must login first!!!`);
+    alert(`you must login first, redirecting you to home page`);
+    setTimeout(go_back,2000)
 }
+
